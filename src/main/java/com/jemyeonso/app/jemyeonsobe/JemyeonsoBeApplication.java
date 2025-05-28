67f5d5ba505bf3188c64a01eb90950ab466e5efe
ywoo121 <ywoo121@naver.com>
2025-05-28 21:14:44 +0900

    Add: SpringBoot initialize

---
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/JemyeonsoBeApplication.java
@@ -0,0 +1,13 @@
+package com.jemyeonso.app.jemyeonsobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemyeonsoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JemyeonsoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jemyeonso/app/jemyeonsobe/JemyeonsoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jemyeonso.app.jemyeonsobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JemyeonsoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
