b8c64d37b26fb6271a5018aad2ded8f7a2850f6b
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-06-01 18:37:39 +0900

    Feat : Document 엔티티

---
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/api/document/entity/Document.java
@@ -0,0 +1,49 @@
+package com.jemyeonso.app.jemyeonsobe.api.document.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "documents")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "type", nullable = false, length = 20)
+    private String type;
+
+    @Column(name = "filename", nullable = false, length = 100)
+    private String filename;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "link", nullable = false, length = 100)
+    private String link;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
