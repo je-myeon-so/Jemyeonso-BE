679d8fe1dd7c2e467fbdc3b721df55078612257f
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-08-20 18:47:34 +0900

    Feat: 회원탈퇴 Repository 메서드 생성

--- src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserRepository.java
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserRepository.java
@@ -3,9 +3,16 @@ package com.jemyeonso.app.jemyeonsobe.api.user.repository;
 import com.jemyeonso.app.jemyeonsobe.api.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByIdAndDeletedAtIsNull(Long id);
+
+    @Query("select u from User u where u.email = :email")
+    Optional<User> findByEmailIncludingDeleted(@Param("email") String email);
+
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
 }
