7931ecb56e195eeb9f3fd434c0f74ef55328285a
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-09-01 12:30:43 +0900

    feat : 주간 점수 초기화

--- src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserRepository.java
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.jemyeonso.app.jemyeonsobe.api.user.repository;
 import com.jemyeonso.app.jemyeonsobe.api.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIncludingDeleted(@Param("email") String email);
 
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.totalScore = 0 WHERE u.deletedAt IS NULL")
+    int resetAllTotalScores();
+
 }
