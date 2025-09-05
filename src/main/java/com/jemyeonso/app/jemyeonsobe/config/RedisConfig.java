b41728d6a848c9c788a29199b3ecc130de63022d
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-09-06 00:17:41 +0900

    fix : 레디스 직렬화 수정

--- src/main/java/com/jemyeonso/app/jemyeonsobe/config/RedisConfig.java
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -11,6 +12,13 @@ public class RedisConfig {
     public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // String 직렬화 설정
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setHashValueSerializer(RedisSerializer.string());
+
         template.afterPropertiesSet();
         return template;
     }
