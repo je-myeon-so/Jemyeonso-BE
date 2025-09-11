7bbf4a20f3c0d18ad9ee46bb7738b69fbb1917dd
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-09-11 23:08:51 +0900

    Chore: ENUM 추가

--- src/main/java/com/jemyeonso/app/jemyeonsobe/common/enums/ErrorMessage.java
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/common/enums/ErrorMessage.java
@@ -22,6 +22,7 @@ public enum ErrorMessage {
 
     // 404 (Not Found)
     USER_NOT_FOUND("존재하지 않는 사용자입니다."),
+    USER_DETAIL_NOT_FOUND("유저 디테일이 존재하지 않습니다."),
     QUESTION_NOT_FOUND("존재하지 않는 질문입니다."),
     INTERVIEW_NOT_FOUND("존재하지 않는 인터뷰입니다.");
 
