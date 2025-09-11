e2b42b713be5e8bbbfc112feed3b7997d20dae3f
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-09-12 03:00:13 +0900

    feat: 전체 사용자 총점 초기화 기능 추가

--- src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserDetailRepository.java
+++ src/main/java/com/jemyeonso/app/jemyeonsobe/api/user/repository/UserDetailRepository.java
@@ -3,10 +3,15 @@ package com.jemyeonso.app.jemyeonsobe.api.user.repository;
 import com.jemyeonso.app.jemyeonsobe.api.user.entity.UserDetail;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
     Optional<UserDetail> findByUserId(Long userId);
 
+    @Modifying
+    @Query("UPDATE UserDetail ud SET ud.totalScore = 0 WHERE ud.totalScore != 0")
+    int resetAllTotalScores();
 }
